9c8a2dc83457f3a33ac4dd1c0066ab455492ba98
Gregg Reynolds <dev@mobileink.com>
2013-08-25 12:02:36 -0500

    msg chg

--- gae/src/gae/user_impl.clj
+++ gae/src/gae/user_impl.clj
@@ -5,7 +5,7 @@
 ;; CAUTION: these routes must match up with the URL pattern specified in
 ;; web.xml for this servlet.
 (defroutes theRouter
-  (GET "/user/hello" [] "<h1>Hi there User from gae.user GAE servlet!</h1>")
+  (GET "/user/hello" [] "<h1>Hi there User from gae.user servlet!</h1>")
   (GET "/user/goodbye" [] "<h1>Goodbye User from gae.user servlet!</h1>")
   (route/not-found "<h1>User page not found</h1>"))
 
