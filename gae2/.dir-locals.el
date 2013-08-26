a41e3496f50253131365e8d489c0981dd28fc707
U-NORC-AD\reynolds-gregg <reynolds-gregg@HPLP0341A.norc.org>
2013-08-26 11:06:37 -0500

    .dir-locals correction

--- gae2/.dir-locals.el
+++ gae2/.dir-locals.el
@@ -2,6 +2,6 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((clojure-mode
-  (warroot . "d:/cygwin/home/Reynolds-Gregg/migae-examples/gae2/war")
-  (srcroot . "d:/cygwin/home/Reynolds-Gregg/migae-examples/gae2/src")))
+  (warroot . "$HOME/migae-examples/gae2/war")
+  (srcroot . "$HOME/migae-examples/gae2/src")))
 
