5d14c785a5e0054cf1ed2d8b80b79734a9628efb
U-NORC-AD\reynolds-gregg <reynolds-gregg@HPLP0341A.norc.org>
2013-08-26 11:03:31 -0500

    add .dir-locals.el for repl hack

---
+++ gae2/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((clojure-mode
+  (warroot . "d:/cygwin/home/Reynolds-Gregg/migae-examples/gae2/war")
+  (srcroot . "d:/cygwin/home/Reynolds-Gregg/migae-examples/gae2/src")))
+
