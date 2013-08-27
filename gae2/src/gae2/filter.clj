95a1c25ee18285c101330c99daa4742fea42f2fa
Gregg Reynolds <dev@mobileink.com>
2013-08-27 02:52:33 -0500

    gae2

--- gae2/src/gae2/filter.clj
+++ gae2/src/gae2/filter.clj
@@ -3,9 +3,6 @@
                           ServletRequest ServletResponse))
   (:gen-class :implements [javax.servlet.Filter]))
 
-;; since we're implementing an interace rather than extending a class,
-;; all methods must be implemented!
-
 (defn -init [^Filter this ^FilterConfig cfg])
 
 (defn -doFilter
@@ -14,10 +11,5 @@
    ^ServletResponse resp
    ^FilterChain chain]
   (do
-    (println "gae2.filter doFilter implementation invoked")
-    (require 'gae2.main-impl 'gae2.user-impl :reload :verbose)
-    ;; if this is the last filter in the chain, the following
-    ;; will invoke the the "target Web resource", i.e. the servlet
-    ;; without this doFilter call, the servlet will not be invoked
-    ;; Remember, we pass the "this" obj as first arg
+    (require 'gae2.main-impl 'gae2.user-impl :reload) ;; :verbose)
     (.doFilter chain rqst resp)))
