cf96bfd01bd486534e3ed44cd9ce4c43f55bc127
Gregg Reynolds <dev@mobileink.com>
2013-08-27 02:11:44 -0500

    compojure2 cleanup

--- compojure2/project.clj
+++ compojure2/project.clj
@@ -4,13 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :javac-options ["-nowarn" "-target" "1.7" "-source" "1.7" "-Xlint:all"]
-  :repl-options {:port 4005}
   :aot [cpj2.main cpj2.user cpj2.filter]
   :compile-path "war/WEB-INF/classes"
-  :resource-paths ["src"] ;; this puts clj source in the jarfile so non-aot methods available
+  :resource-paths ["src"]
   :target-path "war/WEB-INF/lib"
-  :keep-non-project-classes false
-  :omit-source true ;; default
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [ring/ring-servlet "1.2.0"]]
